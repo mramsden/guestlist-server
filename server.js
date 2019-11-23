52b6595eb4d4213d5e650479913963a44db377e4
Marcus Ramsden <marcus@bitsden.com>
2019-11-23 15:06:09 +0000

    Move handlers into their own module

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const { GuestListService } = require('./lib/guest-list-service');
+const { GuestListHandlers } = require('./lib/handlers/guest-list');
 
 const port = process.env.PORT || 3000;
 const service = new GuestListService();
+const guestListHandlers = new GuestListHandlers({ service });
+
 const app = express();
 
-app.use((_req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,POST');
-    res.append('Access-Control-Allow-Headers', ['Content-Type']);
-    next();
-});
-
-app.get('/guestList', (_req, res) => {
-    const guests = service.getGuestList();
-    res.send(guests);
-});
-
-app.post('/guestList', (req, res) => {
-    service.createGuestList();
-    res.status(201)
-        .header('Location', req.url)
-        .send();
-});
+app.get('/guestList', guestListHandlers.getGuestList());
+app.post('/guestList', guestListHandlers.createGuestList());
 
 app.listen(port, () => console.log(`Server listening on :${port}`));
