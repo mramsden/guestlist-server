94f3ee64f6b974cf6fb3f3754d2f5ae3e8683b19
Marcus Ramsden <marcus@bitsden.com>
2019-11-23 16:35:47 +0000

    Add support for authentication using JWTs

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ This is a simple example server that returns a list of guests.
 In order to run this service locally you can run:
 
     npm start
+
+The server will have an error at runtime if you have not created a key pair. In order to create a key pair run the following commands:
+
+    ssh-keygen -t rsa -b 2048 -m PEM -f id_rsa
+    openssl rsa -in id_rsa -pubout -outform PEM -out id_rsa.pub
+
+When prompted for a passphrase make sure to leave it blank.
+
+If you are running in docker you can supply the private and public key using the `TOKEN_PRIVATE_KEY` and `TOKEN_PUBLIC_KEY` environment variables.
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const { GuestListService } = require('./lib/guest-list-service');
 const { GuestListHandlers } = require('./lib/handlers/guest-list');
+const { UserHandlers } = require('./lib/handlers/user');
 
 const port = process.env.PORT || 3000;
 const service = new GuestListService();
 const guestListHandlers = new GuestListHandlers({ service });
+const userHandlers = new UserHandlers();
 
 const app = express();
 
 app.get('/guestList', guestListHandlers.getGuestList());
 app.post('/guestList', guestListHandlers.createGuestList());
+app.post('/sessions', userHandlers.createSession());
 
 app.listen(port, () => console.log(`Server listening on :${port}`));
